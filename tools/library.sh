71ba608977c10bb3c07c32d4c1f9fdc9aea73371
xf0r3m <jakubstasinski@protonmail.com>
2024-01-01 13:02:15 +0100

    Aktualizacja narzędzi immudex-build-menu oraz biblioteki funkcji powłoki library.sh

--- tools/library.sh
+++ tools/library.sh
@@ -3,8 +3,10 @@
 function get_debian_branch() {
   if grep -q 'trixie' /etc/os-release; then
     echo "testing";
-  else
+  elif grep -q 'bookworm' /etc/os-release; then
     echo "stable";
+  else
+    echo "oldstable";
   fi
 }
 
@@ -21,14 +23,14 @@ function check_distro_commit() {
   versionFile="/run/live/medium/live/version";
   if [ -f $versionFile ]; then
     localVersion=$(cat $versionFile);
-    if [ -d /tmp/immudex-testing ]; then
-      $(cd /tmp/immudex-testing && git pull -q);
+    if [ -d /tmp/immudex ]; then
+      $(cd /tmp/immudex && git pull -q);
     else
-      git clone -q https://github.com/xf0r3m/immudex-testing /tmp/immudex-testing;
+      git clone -q https://github.com/xf0r3m/immudex /tmp/immudex;
     fi
-    latestVersion=$(cd /tmp/immudex-testing && git log --pretty=oneline | head -1 | cut -d " " -f 1);
+    latestVersion=$(cd /tmp/immudex && git log --pretty=oneline | head -1 | cut -d " " -f 1);
     if [ "$1" ] && [ "$1" == "--print" ]; then
-      echo "$(cd /tmp/immudex-testing && git log ${localVersion}..${latestVersion})";
+      echo "$(cd /tmp/immudex && git log ${localVersion}..${latestVersion})";
     fi
     if [ "$localVersion" = "$latestVersion" ]; then
       return 0;
